f730d553cd4e6b1ac7af721141695ea18297bb48
dhllim <dhllim@gmail.com>
2026-02-10 14:35:39 +0800

    updated password Password1234

--- app.py
+++ app.py
@@ -1,5 +1,43 @@
 import streamlit as st
 import pandas as pd
+import streamlit as st
+
+
+def check_password():
+    """Returns True if user entered correct password"""
+
+    def password_entered():
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input(
+            "Password",
+            type="password",
+            on_change=password_entered,
+            key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input(
+            "Password",
+            type="password",
+            on_change=password_entered,
+            key="password"
+        )
+        st.error("Password incorrect")
+        return False
+    else:
+        return True
+
+if check_password():
+    st.write("Your protected app content here")
+
+    
+
 
 # Sets the page configuration
 # You can set the page title and layout here
